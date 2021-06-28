143b1ba9ee28a9237ed384ef16d7295c530a4ae7
michaelrlai88 <michaelrlai88@gmail.com>
2021-06-27 17:22:05 -0700

    updated creation links

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,10 +3,26 @@ import styled from 'styled-components';
 import theme from '../theme';
 
 import bannerImageMobile from '../images/mobile/image-interactive.jpg';
+
 import deepEarthMobile from '../images/mobile/image-deep-earth.jpg';
 import nightArcadeMobile from '../images/mobile/image-night-arcade.jpg';
+import soccerTeamMobile from '../images/mobile/image-soccer-team.jpg';
+import theGridMobile from '../images/mobile/image-grid.jpg';
+import fromAboveMobile from '../images/mobile/image-from-above.jpg';
+import pocketBorealisMobile from '../images/mobile/image-pocket-borealis.jpg';
+import curiosityMobile from '../images/mobile/image-curiosity.jpg';
+import fisheyeMobile from '../images/mobile/image-fisheye.jpg';
 
 import bannerImageDesktop from '../images/desktop/image-interactive.jpg';
+
+import deepEarthDesktop from '../images/desktop/image-deep-earth.jpg';
+import nightArcadeDesktop from '../images/desktop/image-night-arcade.jpg';
+import soccerTeamDesktop from '../images/desktop/image-soccer-team.jpg';
+import theGridDesktop from '../images/desktop/image-grid.jpg';
+import fromAboveDesktop from '../images/desktop/image-from-above.jpg';
+import pocketBorealisDesktop from '../images/desktop/image-pocket-borealis.jpg';
+import curiosityDesktop from '../images/desktop/image-curiosity.jpg';
+import fisheyeDesktop from '../images/desktop/image-fisheye.jpg';
 
 import { MenuContext } from '../context/MenuContext';
 
@@ -18,7 +34,7 @@ const MainContainer = styled.div`
     padding: 180px 24px 0 24px;
   }
   @media screen and (min-width: 1024px) {
-    padding: 0 165px;
+    padding: 160px 165px 0 165px;
   }
 `;
 
@@ -29,48 +45,62 @@ const Contents = styled.div`
   }
 `;
 
-const Banner = styled.div``;
+const Banner = styled.div`
+  position: relative;
+`;
 
 const BannerImageContainer = styled.div`
-  img {
-    width: 100%;
-  }
-  @media screen and (max-width: 1023px) {
+  @media screen and (max-width: 1439px) {
     margin-bottom: 50px;
   }
 `;
 
 const BannerImageMobile = styled.img`
   display: none;
-  @media screen and (max-width: 1023px) {
+  width: 100%;
+  @media screen and (max-width: 1439px) {
     display: block;
   }
 `;
 
 const BannerImageDesktop = styled.img`
   display: none;
-  @media screen and (min-width: 1024px) {
+  width: 730px;
+  margin-bottom: 185px;
+  @media screen and (min-width: 1440px) {
     display: block;
   }
 `;
 
-const Text = styled.div``;
+const Text = styled.div`
+  @media screen and (min-width: 1440px) {
+    position: absolute;
+    bottom: -1px;
+    right: 0;
+    background-color: ${white};
+    height: 315px;
+    width: 545px;
+    padding-left: 95px;
+    padding-top: 95px;
+  } ;
+`;
 
 const Title = styled.div`
-  @media screen and (max-width: 1023px) {
+  color: ${black};
+  text-transform: uppercase;
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 100;
+  font-size: 48px;
+  margin-bottom: 30px;
+  @media screen and (max-width: 1439px) {
     text-align: center;
-    color: ${black};
-    text-transform: uppercase;
-    font-family: 'Josefin Sans', sans-serif;
-    font-weight: 100;
-    font-size: 36px;
     margin-bottom: 32px;
   }
 `;
 
 const Para = styled.div`
   color: ${darkGray};
-  @media screen and (max-width: 1023px) {
+  @media screen and (max-width: 1439px) {
     text-align: center;
     padding: 26px;
     margin-bottom: 100px;
@@ -103,7 +133,7 @@ const CreationsLinksMobile = styled.div`
     font-family: 'Josefin Sans', sans-serif;
     font-weight: 100;
     font-size: 24px;
-    width: 120px;
+    width: 140px;
     z-index: 1;
     bottom: 24px;
     left: 24px;
@@ -112,15 +142,46 @@ const CreationsLinksMobile = styled.div`
     width: 100%;
     filter: brightness(0.7);
     display: block;
+    position: relative;
   }
-  @media screen and (max-width: 1023px) {
+
+  @media screen and (max-width: 1439px) {
     display: block;
   }
 `;
 
-const DeepEarth = styled.div``;
+const CreationsLinksDesktop = styled.div`
+  display: none;
+  justify-content: space-between;
+  div {
+    position: relative;
+  }
 
-const NightArcade = styled.div``;
+  h3 {
+    color: ${white};
+    text-transform: uppercase;
+    position: absolute;
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 100;
+    font-size: 24px;
+    width: 140px;
+    z-index: 1;
+    bottom: 44px;
+    left: 24px;
+  }
+  img {
+    width: 255px;
+    filter: brightness(0.7);
+    display: block;
+    padding-bottom: 24px;
+    position: relative;
+  }
+
+  @media screen and (min-width: 1440px) {
+    display: flex;
+    flex-wrap: wrap;
+  }
+`;
 
 const Main = () => {
   const { menuOpen, setMenuOpen } = useContext(MenuContext);
@@ -148,13 +209,103 @@ const Main = () => {
           <CreationsLinksMobile>
             <div>
               <h3>Deep earth</h3>
-              <img src={deepEarthMobile} alt='' />
+              <a href='/'>
+                <img src={deepEarthMobile} alt='' />
+              </a>
             </div>
             <div>
               <h3>Night arcade</h3>
-              <img src={nightArcadeMobile} alt='' />
+              <a href='/'>
+                <img src={nightArcadeMobile} alt='' />
+              </a>
+            </div>
+            <div>
+              <h3>Soccer team vr</h3>
+              <a href='/'>
+                <img src={soccerTeamMobile} alt='' />
+              </a>
+            </div>
+            <div>
+              <h3>The grid</h3>
+              <a href='/'>
+                <img src={theGridMobile} alt='' />
+              </a>
+            </div>
+            <div>
+              <h3>From up above vr</h3>
+              <a href='/'>
+                <img src={fromAboveMobile} alt='' />
+              </a>
+            </div>
+            <div>
+              <h3>Pocket borealis</h3>
+              <a href='/'>
+                <img src={pocketBorealisMobile} alt='' />
+              </a>
+            </div>
+            <div>
+              <h3>The curiosity</h3>
+              <a href='/'>
+                <img src={curiosityMobile} alt='' />
+              </a>
+            </div>
+            <div>
+              <h3>Make it fisheye</h3>
+              <a href='/'>
+                <img src={fisheyeMobile} alt='' />
+              </a>
             </div>
           </CreationsLinksMobile>
+          <CreationsLinksDesktop>
+            <div>
+              <h3>Deep earth</h3>
+              <a href='/'>
+                <img src={deepEarthDesktop} alt='' />
+              </a>
+            </div>
+            <div>
+              <h3>Night arcade</h3>
+              <a href='/'>
+                <img src={nightArcadeDesktop} alt='' />
+              </a>
+            </div>
+            <div>
+              <h3>Soccer team vr</h3>
+              <a href='/'>
+                <img src={soccerTeamDesktop} alt='' />
+              </a>
+            </div>
+            <div>
+              <h3>The grid</h3>
+              <a href='/'>
+                <img src={theGridDesktop} alt='' />
+              </a>
+            </div>
+            <div>
+              <h3>From up above vr</h3>
+              <a href='/'>
+                <img src={fromAboveDesktop} alt='' />
+              </a>
+            </div>
+            <div>
+              <h3>Pocket borealis</h3>
+              <a href='/'>
+                <img src={pocketBorealisDesktop} alt='' />
+              </a>
+            </div>
+            <div>
+              <h3>The curiosity</h3>
+              <a href='/'>
+                <img src={curiosityDesktop} alt='' />
+              </a>
+            </div>
+            <div>
+              <h3>Make it fisheye</h3>
+              <a href='/'>
+                <img src={fisheyeDesktop} alt='' />
+              </a>
+            </div>
+          </CreationsLinksDesktop>
         </Creations>
       </Contents>
     </MainContainer>
