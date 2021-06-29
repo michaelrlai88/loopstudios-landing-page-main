477850fa7ab186d9981915c1ede57d26cd0b4210
michaelrlai88 <michaelrlai88@gmail.com>
2021-06-29 00:15:26 -0700

    updated burger icon active state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,7 +16,6 @@ const HeaderContainer = styled.div`
 
 const Hero = styled.div`
   height: 100%;
-  width: 100%;
 
   background-size: cover;
   background-position: 50% 0;
@@ -184,11 +183,29 @@ const Header = () => {
             <NavBar menuOpen={menuOpen}>
               <NavLogo menuOpen={menuOpen}></NavLogo>
               <Burger onClick={openMenu} menuOpen={menuOpen}>
-                <svg width='24' height='16' xmlns='http://www.w3.org/2000/svg'>
-                  <g fill='#FFF' fill-rule='evenodd'>
-                    <path d='M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z' />
-                  </g>
-                </svg>
+                {menuOpen ? (
+                  <svg
+                    width='20'
+                    height='20'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z'
+                      fill='#FFF'
+                      fill-rule='evenodd'
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    width='24'
+                    height='16'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <g fill='#FFF' fill-rule='evenodd'>
+                      <path d='M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z' />
+                    </g>
+                  </svg>
+                )}
               </Burger>
             </NavBar>
             <NavMenu menuOpen={menuOpen}>
